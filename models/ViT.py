aba6de82a1c6f67640c0c774ecb65b6cc7d40683
JuWan <aodwndhks@gmail.com>
2023-03-16 16:54:10 +0900

    make ViT

--- models/ViT.py
+++ models/ViT.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchsummary import summary
 
 from einops import rearrange, reduce, repeat
 from einops.layers.torch import Rearrange, Reduce
@@ -76,12 +77,72 @@ class MultiHeadAttention(nn.Module):
         # #torch.Size([16, 197, 768])
         return out
         
+class ResidualAdd(nn.Module):
+    def __init__(self,fn):
+        super().__init__()
+        self.fn=fn
         
-        
-        
+    def forward(self,x,**kwargs):
+        res=x
+        x=self.fn(x,**kwargs)  # ex_ self.fn은 Sequential같은 함수이다
+        x+=res
+        return x
     
 
-    
+# Subclassing nn.Sequential to avoid writing the forward method
+class FeedForwardBlock(nn.Sequential):
+    def __init__(self,emb_size,expansion=4,drop_p=0.):
+        super().__init__(
+            nn.Linear(emb_size,expansion*emb_size),
+            nn.GELU(),
+            nn.Dropout(drop_p),
+            nn.Linear(expansion*emb_size,emb_size)
+        )   
+
+        
+        
+# create Transformer Encoder Block
+class TransformerEncoderBlock(nn.Sequential):
+    def __init__(self,emb_size=768,drop_p=0.,forward_expansion=4,forward_drop_p=0.,**kwargs):
+        super().__init__(
+            ResidualAdd(nn.Sequential(
+                nn.LayerNorm(emb_size),
+                MultiHeadAttention(emb_size,**kwargs),
+                nn.Dropout(drop_p)
+            )),
+            ResidualAdd(nn.Sequential(
+                nn.LayerNorm(emb_size),
+                FeedForwardBlock(emb_size,expansion=forward_expansion,
+                                 drop_p=forward_drop_p),
+                nn.Dropout(drop_p)
+            ))
+        )     
+        
+
+class TransformerEncoder(nn.Sequential):
+    def __init__(self,depth=12,**kwargs):
+        super().__init__(*[TransformerEncoderBlock(**kwargs) for _ in range(depth)])
+        
+class ClassificationHead(nn.Sequential):
+    def __init__(self,emb_size=768,n_classes=10):
+        super().__init__(
+            Reduce('b n e -> b e',reduction='mean'),
+            nn.LayerNorm(emb_size),
+            nn.Linear(emb_size,n_classes)
+        )
+        
+class ViT(nn.Sequential):
+    def __init__(self,in_channels=3,patch_size=16,emb_size=768,img_size=224,depth=12,n_classes=10,**kwargs):
+        super().__init__(
+            PatchEmbedding(in_channels,patch_size,emb_size,img_size),
+            TransformerEncoder(depth,emb_size=emb_size,**kwargs),
+            ClassificationHead(emb_size,n_classes)
+        )
+
+
+
+
+
 
 if __name__=='__main__':
     device='cuda:0'
@@ -95,3 +156,33 @@ if __name__=='__main__':
     MHA_output = MHA(patch_output)
     print(MHA_output.shape)
     
+    # check feedforwardblock
+    x=torch.randn(16,1,128).to(device)
+    model=FeedForwardBlock(128).to(device)
+    output=model(x)
+    print(output.shape)
+    
+    # Check TransformerEncoderBlock
+    model = TransformerEncoderBlock().to(device)
+    output = model(patch_output).to(device)
+    print(output.shape)
+    
+    
+    # Check TransformerEncoder
+    model = TransformerEncoder().to(device)
+    output = model(patch_output)
+    print(output.shape)
+    
+    # check
+    x = torch.randn(16, 1, 768).to(device)
+    model = ClassificationHead().to(device)
+    output = model(x)
+    print(output.shape)
+    
+    # Check
+    x = torch.randn(16,3,224,224).to(device)
+    model = ViT().to(device)
+    output = model(x)
+    print(output.shape)
+    
+    print(summary(model,(3,224,224),device='cuda'))
