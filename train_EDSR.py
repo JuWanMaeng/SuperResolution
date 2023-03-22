8f55f384b809653a1cc5b37ce32b54f0c4e6f87b
JuWan <aodwndhks@gmail.com>
2023-03-22 21:21:32 +0900

    remedy train_EDSR.py

--- train_EDSR.py
+++ train_EDSR.py
@@ -13,7 +13,13 @@ from tqdm import tqdm
 from utils.utils import calculate_psnr,calculate_ssim
 from einops import rearrange
 from torchsummary import summary
+import copy
 
+
+# get current lr
+def get_lr(opt):
+    for param_group in opt.param_groups:
+        return param_group['lr']
 
 
 
@@ -38,19 +44,25 @@ def main(scale=0):
     
     optimizer=optim.Adam(model.parameters(),lr=10e-4,betas=[0.9,0.999],eps=10e-8)
     criterion=nn.L1Loss()
-    scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[200,400,600,800], gamma=0.5)
+    scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[180,360,540,720],gamma=0.5)
     step=0
     best_loss=float('inf')
     
 
-    for epoch in range(125000): 
+    for epoch in range(1,801): 
         current_lr=optimizer.param_groups[0]['lr']
-        print('-'*20)
-        print(f'epoch:{epoch+1}, current_lr:{current_lr}, iter:{step}')
+        print('-'*60)
+        print(f'epoch:{epoch}, current_lr:{current_lr}, iter:{step}')
+        
+        if current_lr != get_lr(optimizer):   # lr이 감소하지 않은 상태면 이전 학습된 모델들중 제일 좋았던 모델을 불러온다.
+            print('Loading best model weights!')
+            model.load_state_dict(best_model_wts)
+        
+        
         model.train()
         train_tq=tqdm(train_dataloader, ncols=80, smoothing=0, bar_format='train: {desc}|{bar}{r_bar}')
         for imgs in train_tq:
-            step+=1
+            step+=64                            # 이미지 한장을 1 step이라고 하자
             HR_img,LR_img=imgs['H'],imgs['L']
             HR_img=HR_img.to(device)
             LR_img=LR_img.to(device)
@@ -63,44 +75,54 @@ def main(scale=0):
             optimizer.step()
         scheduler.step()        
         
-        if step%390==0:
-            current_loss=0
-            psnr=0
-            ssim=0
-            model.eval()
-            val_tq=tqdm(val_dataloader, ncols=80, smoothing=0, bar_format='val: {desc}|{bar}{r_bar}')
-            with torch.no_grad():
-                for idx,imgs in enumerate(val_tq):
-                    if idx==10:
-                        break
-                    HR_img=imgs['H'].to(device)
-                    LR_img=imgs['L'].to(device)
-                    
-                    output_img=model(LR_img)
-                    current_loss+=criterion(output_img,HR_img)
-                    
-                    output_img=output_img[0].cpu().numpy()
-                    HR_img=HR_img[0].cpu().numpy()
-                    output_img=(rearrange(output_img,'c h w -> h w c'))
-                    HR_img=(rearrange(HR_img,'c h w -> h w c'))
+        
+        current_loss=0
+        psnr=0
+        ssim=0
+        model.eval()
+        val_tq=tqdm(val_dataloader, ncols=80, smoothing=0, bar_format='val: {desc}|{bar}{r_bar}')
+        with torch.no_grad():
+            for idx,imgs in enumerate(val_tq):
+                if idx==10:                   # val은 10장만
+                    break
+                HR_img=imgs['H'].to(device)
+                LR_img=imgs['L'].to(device)
+                
+                output_img=model(LR_img)
+                current_loss+=criterion(output_img,HR_img)
+                
+                output_img=output_img[0].cpu().numpy()
+                HR_img=HR_img[0].cpu().numpy()
+                output_img=(rearrange(output_img,'c h w -> h w c'))
+                HR_img=(rearrange(HR_img,'c h w -> h w c'))
 
-                    psnr+=calculate_psnr(output_img,HR_img,crop_border=8)
-                    ssim+=calculate_ssim(output_img,HR_img,crop_border=8)
-                    
+                psnr+=calculate_psnr(output_img,HR_img,crop_border=8)
+                ssim+=calculate_ssim(output_img,HR_img,crop_border=8)
+                
+        
+        epoch_loss=current_loss / 10
+        
+        
+        if epoch_loss < best_loss:
+            best_loss = epoch_loss
+            best_model_wts = copy.deepcopy(model.state_dict())
+            torch.save(model.state_dict(), 'experiment/EDSR/best.pt')
+            print('Copied best model weights!')
+
+        if current_lr != get_lr(optimizer):
+            print('Loading best model weights!')
+            model.load_state_dict(best_model_wts)
             
-            epoch_loss=current_loss / 10
-            
-            if best_loss > epoch_loss:
-                torch.save(model.state_dict(),f'experiment/EDSR/best.pt')
+           
                 
                 
-            avg_psnr=psnr / 10
-            avg_ssim=ssim / 10
-            psnr_list=np.load('psnr.npy').tolist()
-            psnr_list.append(avg_psnr)
-            np.save('psnr',psnr_list)
-            
-            print(f'epoch:{epoch+1}, iter:{step}, Average PSNR:{avg_psnr:.4f}, Average SSIM:{avg_ssim:.4f}, loss:{epoch_loss:.4f}\n')
+        avg_psnr=psnr / 10
+        avg_ssim=ssim / 10
+        psnr_list=np.load('psnr.npy').tolist()
+        psnr_list.append(avg_psnr)
+        np.save('psnr',psnr_list)
+        
+        print(f'epoch:{epoch}, iter:{step}, Average PSNR:{avg_psnr:.4f}, Average SSIM:{avg_ssim:.4f}, loss:{epoch_loss:.4f}\n')
         
 
 if __name__ == '__main__':
